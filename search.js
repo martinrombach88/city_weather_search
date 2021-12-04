6857d2346e8e39c04110b7d3a936f3d27eb43f86
= <martinrombach88@gmail.com>
2021-12-04 11:57:06 +0900

    Add weather data code
    I've practiced making this weather application twice during my bootcamp. This is my second try of the code, copied and integrated into GitHub.
    
    The for loop in filterWeatherData was learnt from my camp's lecturer. It took some analysis to really understand that i+=8 means that we are jumping across one day. The weather data has an associative array for 8 times in the day, so increasing the iteration by 8 and mutating the number is a great approach from her.
    
    Integrating weatherArray into the loop was my mentor's idea. i becomes the index for the day to receive data from, and j becomes the index for each associative array within the returned weather object. It's really quite clever.
    
    I broke down his code, wrote a how to do guide and then rebuilt the code for myself. filterWeatherData is the closest to his original design.
    
    The key thing I learned in the second try was to use objects. My mentor gave me his version of the code, and this my attempt to use his ideas and structure (which follow an MVC structure and filter arguments through 6 different functions).
    
    While I found it too difficult to build the setToday/createDayDefault/setOtherDay functions exactly the way of my mentors but I did use many of his ideas, in particular his use of arguments.
    
    My bootcamp lecturer showed us a build she did all the assignment and item creation was built through loops and id assignment, but I found objects an easier concept to grasp, and she encouraged me to try it.
    
    With the DOM elements, it seems key to try and avoid repetitive code as much as possible. createItem and createIcon are great tools that I found very useful for feeding arguments into. By using tags and content as an argument, I can feed in whatever data I need.
    
    With the day functions setToday, setOtherDay, setTodayContainer etc, it was very useful to use the parent as an argument. by adding the parent on call of the function, you can assign the parent repeatedly inside the function.
    
    Overall this was a satisfying thing to build.
    
    For GitHub, I'm focusing on added functionality. I've styled the app in a way I like, but I hope to go further and add the background changing feature from the Star Wars quotes app too.

--- search.js
+++ search.js
@@ -6,21 +6,25 @@ search.addEventListener("focus", function(e) {
 
 search.addEventListener("keyup", function(e) {
     if(e.key == 'Enter') {
-        console.log(e.target.value);
         getWeather(e.target.value);
     }
 })
 
+//Getting the Data
 
-function getWeather(city) {
+function getWeather(city)  {
+    const results = document.querySelector("#results");
     const APP_ID = "1cbfb739f9b6cfae7ea0cc16fe258306";
     let xhr = new XMLHttpRequest();
     let citySelect = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid="+ APP_ID;
     xhr.open('GET', citySelect);
     xhr.addEventListener('load', function() {
         if (xhr.status == 200) {
+            results.innerHTML = "";
             let weatherResponse = JSON.parse(xhr.response);
-            console.log(weatherResponse);
+            let filteredWData = filterWeatherData(weatherResponse.city.name, weatherResponse.list);
+            setToday(results, filteredWData);
+            setOtherContainer(results, filteredWData);
         }
     })
     xhr.send(null);
@@ -32,3 +36,103 @@ function getDayName(dateStr, locale)
     return date.toLocaleDateString(locale, { weekday: 'long' });        
 }
 
+
+function filterWeatherData(city, dataList) {
+    let weatherArray = [];
+    let j = 0;
+
+    for (let i = 1; i < dataList.length; i+=8) {
+        weatherArray[j] = {
+            city: city, 
+            icon: dataList[i].weather[0].icon,
+            day: getDayName(dataList[i].dt_txt),
+            description: dataList[i].weather[0].description,
+            temperature: dataList[i].main.temp,
+            wind: dataList[i].wind.speed, 
+            pressure: dataList[i].main.pressure,
+            humidity: dataList[i].main.humidity,
+            cloudiness: dataList[i].clouds.all
+        }
+        j++
+    }
+    
+    return weatherArray;
+}
+
+//Create HTML and Set Data
+
+//Create Default Values Shared by All Days
+function createDayDefault(parent, data, index) {
+    let day = createItem('h3', data[index].day),
+    desc = createItem('p', data[index].description),
+    temp = createItem('p', `temperature: ${(data[index].temperature - 273.15).toFixed(1)}°`);
+    parent.appendChild(day);
+    parent.appendChild(desc);
+    parent.appendChild(temp);
+}
+
+//Create HTML Structure - Today and Container
+function setToday(parent, dataList) {
+    //Container
+    let todayContainer = createItem('div');
+    todayContainer.classList.add('today', 'container');
+    parent.appendChild(todayContainer);
+
+    //City, Icon, Default Values
+    let city = createItem('h2', dataList[0].city);
+    let icon = createIcon(dataList[0].icon);
+    todayContainer.appendChild(city);
+    todayContainer.appendChild(icon);
+    createDayDefault(todayContainer, dataList, 0);
+
+    //Inner Div
+    let tInnerDiv = createItem('div');
+    tInnerDiv.classList.add('innerDiv');
+    tInnerDivSide1 = createItem('div'),
+    tInnerDivSide2 = createItem('div'),
+    wind = createItem('p', `wind speed: ${dataList[0].wind} m/s`),
+    pres = createItem('p', `pressure: ${dataList[0].pressure}hPa`),
+    humi = createItem('p', `humidity: ${dataList[0].humidity}%`),
+    clou = createItem('p', `cloudiness: ${dataList[0].cloudiness}%`);
+
+    //Append Child
+    todayContainer.appendChild(tInnerDiv);
+    tInnerDiv.appendChild(tInnerDivSide1);
+    tInnerDiv.appendChild(tInnerDivSide2);
+    tInnerDivSide1.appendChild(wind);
+    tInnerDivSide1.appendChild(pres);
+    tInnerDivSide2.appendChild(humi);
+    tInnerDivSide2.appendChild(clou);
+}
+
+//Create HTML Structure - Other Days and Container
+function setOtherContainer(parent, data) {
+    let otherDaysContainer = createItem('div');
+    otherDaysContainer.classList.add('allOther', 'container');
+    parent.appendChild(otherDaysContainer);
+    for (let i = 1; i <= 4; i++){
+        setOtherDay(otherDaysContainer, data, i);
+    }
+}
+
+function setOtherDay(parent, dataList, index) {
+    let otherDayContainer = createItem('div');
+    otherDayContainer.classList.add('otherDay');
+    let icon = createIcon(dataList[index].icon);
+    parent.appendChild(otherDayContainer);
+    createDayDefault(otherDayContainer, dataList, index);
+    otherDayContainer.insertBefore(icon, otherDayContainer.firstChild.nextSibling);   
+}
+
+//Create Individual Elements and Icons
+function createItem(tag, data) {
+    let item = document.createElement(tag);
+    item.textContent = data;
+    return item;   
+}
+
+function createIcon(data) {
+    let icon = document.createElement('img'); // Should be skeleton Phase
+    icon.setAttribute("src", "http://openweathermap.org/img/w/" + data + ".png"); //Should be Populate phase
+    return icon; 
+}
